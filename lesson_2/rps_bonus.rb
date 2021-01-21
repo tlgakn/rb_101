20cb51bbd02d778eac5a5027aaa1fb6f2ca58ba1
Tolga Akin <tolgaakin737@icloud.com>
2021-01-21 17:25:37 +0100

    adding rps_bonus.rb

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -1,118 +1,173 @@
 =begin
 
-Here are a couple of bonus features for the Rock Paper Scissors program.
+CREATING A RPS GAME WITH ADDITIONAL FEATURES (RB_101 RPS BONUS ASSIGNMENTS)
 
-Lizard Spock
+# SET
+- set VALID_CHOICES 
+- set WIN_COMBINATIONS
+- set WIN_SCORE
+- set score_board
 
-3) Keep score of the player's and computer's wins. When either the player
-or computer reaches five wins, the match is over, and the winning player
-becomes the grand winner. Don't add your incrementing logic to display_results.
-Keep your methods simple; they should perform one logical task.
-no more, no less.
+# in the beginning
+- display a welcome text and the rules of the programm
 
-algorithm for third question (keeping score of the player's and
-computer's wins)
+# in the programm
+- display the chose options 
+- ask the user to choose one (r,p, sc, sp, l)
+      if the user enters a non valid choice
+          display an error message ex: "That's not a valid choice"
+- display in every round which user chose which object
+    ex: You chose: r; computer chose: sp
+- display the score
+    ex: your score: 0, computer score: 1
+- keep updating the score
+- when a user reaches 5 
+    ask the user if he wants to play again
+        - ask to enter 'y/Y' to play again, and anything else to exit
+- display a than you message for using the calculator
 
+algorithm:
+- define the helper methods and set the variables/constants
+- figure out a logic how the program should run
 
-- create a hash with player/computer keys SCORE
-- create a method UPDATE_VALUE to increment the SCORE every time by 1 on every win
-  whose value will incremented every time by 1 by each win
-- the program will stop when one of the player reach 5
-- make an if an else statement an increment the score hash every time by one each time who wins
 
 =end
 
 VALID_CHOICES = ['r', 'p', 'sc', 'sp', 'l']
 
+WIN_SCORE = 5
+
+WIN_COMBINATIONS = {
+  'r' => ['l', 'sc'],
+  'l' => ['p', 'sp'],
+  'sp' => ['r', 'sc'],
+  'sc' => ['l', 'p'],
+  'p' => ['r', 'sp']
+}
+
+score_board = {
+  player: 0,
+  computer: 0
+}
+
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts "=> #{message}"
 end
 
-intro = <<-MSG
-
-You can just use the first character of your choice.  
-For example, just enter 'r' for 'rock'"
-
-
-Paper = p
-Scissors = sc
-Spock = sp
-Lizard = l
-
-MSG
-prompt(intro)
-
-LOOKUP_HASH = {
-
-  'r' => 'rock',
-  'p' => 'paper',
-  'sc' => 'scissors',
-  'sp' => 'spock',
-  'l' => 'lizard'
-}
-
-# made the words shortcut versions of one/two letter in the beginning
-WIN_HASH = {
-  'r' => %w(l sc),
-  'p' => %w(r sp),
-  'sc' => %w(p l),
-  'sp' => %w(r sc),
-  'l' => %w(sp p)
-}
-
-UPDATE_SCORE = {
-  'player' => 0,
-  'computer' => 0
-}
-
 def win?(first, second)
-  if WIN_HASH[first].include?(second)
-    SCORE_HASH['player'] += 1
-  else
-    SCORE_HASH['computer'] += 1
+  WIN_COMBINATIONS[first].include?(second)
+end
+
+def update_score_board(player, computer, score_board)
+  if win?(player, computer)
+    score_board[:player] += 1
+  elsif win?(computer, player)
+    score_board[:computer] += 1
   end
 end
 
-# def win?(first, second)
-#   if WIN_HASH[first].include?(second)
-#     true
-#   else
-#     false
-#   end
-# end
+def reset_score_board(score_board)
+  score_board[:computer] = 0
+  score_board[:player] = 0
+end
+
+def display_score_board(score_board)
+  "Your score: #{score_board[:player]}.
+  Computer score: #{score_board[:computer]}."
+end
 
 def display_results(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    "You won!"
   elsif win?(computer, player)
-    prompt("Computer won!")
+    "Computer won!"
   else
-    prompt("It's a tie!")
+    "It's a tie!"
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp().downcase
+def match_ended?(score_board)
+  score_board[:player] == WIN_SCORE || score_board[:computer] == WIN_SCORE
+end
 
-    if VALID_CHOICES.include?(choice)
+def display_match_winner(score_board)
+  if score_board[:player] == WIN_SCORE
+    "Congrats! You won #{WIN_SCORE} matches, You are the winner!"
+  elsif score_board[:computer] == WIN_SCORE
+    "Computer won #{WIN_SCORE} matches, Computer is the winner :("
+  end
+end
+
+def display_choice_options
+  "Choose one:
+  'r'  for rock,
+  'p'  for paper,
+  'sc' for scissors,
+  'sp' for spock,
+  'l'  for lizard"
+end
+
+def display_rules
+  <<-INTRO
+  Welcome to the game of Rock, Paper Scissors, Spock, Lizard!
+  The rules are:
+  Rock crushes Lizard and crushes Scissors.
+  Paper covers Rock and disproves Spock.
+  Scissors cut Paper and decapitates Lizard.
+  Spock smashes Scissors and vaporizes Rock.
+  Lizard poisons Spock and eats Paper.
+  You must win #{WIN_SCORE} matches to win the entire game.
+  INTRO
+end
+
+def clear_screen
+  system('clear')
+end
+
+prompt(display_rules)
+
+loop do
+  loop do
+    choice = '' # choice is initialized outside of the most inner loop
+    loop do # most inner loop for input validation
+      prompt(display_choice_options)
+      choice = gets.chomp.downcase
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+        sleep(2)
+        clear_screen
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample # computer choses a random object
+
+    puts "You chose: #{choice}; Computer chose: #{computer_choice}" # displaying the choices
+
+    prompt(display_results(choice, computer_choice)) 
+
+    update_score_board(choice, computer_choice, score_board)
+
+    prompt(display_score_board(score_board))
+
+    sleep(2)
+    clear_screen
+
+    if match_ended?(score_board)
+      prompt(display_match_winner(score_board))
+      reset_score_board(score_board)
       break
-    else
-      prompt("That's not a valid choice")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{LOOKUP_HASH[choice]};
-        Computer chose: #{LOOKUP_HASH[computer_choice]}")
-
-  display_results(choice, computer_choice)
-
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  prompt(
+    "Do you want to play again?
+    Enter Y/y to play again. Enter anything else to exit."
+  )
+  answer = gets.chomp
+  break unless answer.downcase == 'y'
 end
-prompt("Thank you for playing. Good bye!")
+
+prompt("Thank you for playing. Goodbye!")
