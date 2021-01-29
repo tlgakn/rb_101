5eb6187f0b0dcc820989e5b0fdeac42deda374a6
Tolga Akin <tolgaakin737@icloud.com>
2021-01-29 17:03:00 +0100

    adding fibonacci problems to 01_medium

---
+++ small_problems/01_medium/09_fibonacci_procedural.rb
@@ -0,0 +1,39 @@
+=begin
+Fibonacci Numbers (Procedural)
+In the previous exercise, we developed a recursive solution to calculating the nth Fibonacci number. In a language that is not optimized for recursion, some (not all) recursive methods can be extremely slow and require massive quantities of memory and/or stack space.
+
+Ruby does a reasonably good job of handling recursion, but it isn't designed for heavy recursion; as a result, the Fibonacci solution is only useful up to about fibonacci(40). With higher values of nth, the recursive solution is impractical. 
+(Our tail recursive solution did much better, but even that failed at around fibonacci(8200).)
+
+Fortunately, every recursive method can be rewritten as a non-recursive (procedural) method.
+
+Rewrite your recursive fibonacci method so that it computes its results without recursion.
+=end
+
+# Rona's Solution
+def fibonacci(number)
+  array = [0, 1, 1]
+  index = 3
+  return 1 if number <= 2
+  
+  loop do
+    array[index] = array[index - 1] + array[index - 2]
+    break if index == number
+    index += 1
+  end
+  array[number]
+end
+
+# LS Solution
+def fibonacci(nth)
+  first, last = [1, 1]
+  3.upto(nth) do
+    first, last = [last, first + last]
+  end
+
+  last
+end
+
+p fibonacci(20) #== 6765
+p fibonacci(100) == 354224848179261915075
+p fibonacci(100_001) # => 4202692702.....8285979669707537501
