87633ea2308ab86a152ac8f55fbc71cdae97694d
Tolga Akin <tolgaakin737@icloud.com>
2021-07-26 10:36:02 +0200

    added second solution for sum_square problem

--- small_problems/02_medium/00_sum_square.rb
+++ small_problems/02_medium/00_sum_square.rb
@@ -23,3 +23,29 @@ def sum_square_difference(n)
 
   sum**2 - sum_of_squares
 end
+
+# second time
+def sum_square(num)
+  sum = 0
+  1.upto(num) do |n|
+    sum += n
+  end
+  
+  sum_square = sum * sum
+end
+
+def square_sum(num)
+  result = []
+  
+  1.upto(num) do |num|
+    result << num*num
+  end
+  result.sum
+end
+
+
+def sum_square_difference(num)
+  sum_square = sum_square(num)
+  square_sum = square_sum(num)
+  sum_square - square_sum
+end
